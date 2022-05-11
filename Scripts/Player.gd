7c4c0bd9862fa984e3ee46b72ae8b537fe83d167
Pedro Camboim <72642597+pedrxlz@users.noreply.github.com>
2022-05-11 17:07:38 -0300

    Walk sprites and mechanics added

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 var velocity : Vector2 = Vector2()
 var direction : Vector2 = Vector2()
+var walk_speed = 100
+
+onready var sprite = $AnimatedSprite
 
 func read_input():
 	velocity = Vector2()
@@ -9,21 +12,42 @@ func read_input():
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 		direction = Vector2(0, -1)
-		
+				
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
-		direction = Vector2(0, -1)
-		
+		direction = Vector2(0, 1)
+				
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
-		direction = Vector2(0, -1)
+		direction = Vector2(-1, 0)
 		
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
-		direction = Vector2(0, -1)
-
+		direction = Vector2(1, 0)
+		
+	update_animation()	
+		
 	velocity = velocity.normalized()
-	velocity = move_and_slide(velocity * 200)
+	velocity = move_and_slide(velocity * walk_speed)
 	
 func _physics_process(delta):
 	read_input()
+
+func update_animation():
+	if velocity.x > 0 :
+		sprite.play('walk-right')
+	elif velocity.x < 0:
+		sprite.play('walk-left')
+	elif velocity.y > 0:
+		sprite.play('walk-down')
+	elif velocity.y < 0:
+		sprite.play('walk-up')
+		
+	elif direction.x == 1:
+		sprite.play('idle-right')
+	elif direction.x == -1:
+		sprite.play('idle-left')
+	elif direction.y == -1:
+		sprite.play('idle-up')
+	elif direction.y == 1:
+		sprite.play('idle-down')
