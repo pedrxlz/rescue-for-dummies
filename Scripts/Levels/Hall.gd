97a04b3092b9105336cf2f602695e1e92e4846c9
Pedro Camboim <72642597+pedrxlz@users.noreply.github.com>
2022-05-31 17:16:58 -0300

    Add files via upload

--- Scripts/Levels/Hall.gd
+++ Scripts/Levels/Hall.gd
@@ -2,23 +2,38 @@ extends Node2D
 
 var area_active = [1, false]
 
-func ready():
+func _ready():
 	Global.player2D = true
+	
+	match Global.game_progress:
+		1:
+			get_node('Player').position.x = 365
+		2:
+			get_node('Player').position.x = 836.7
+		3:
+			get_node('Player').position.x = 1312
+		4:
+			get_node('Player').position.x = 1790
 	
 	
 func _physics_process(delta):
 	if area_active[1] and (Input.is_action_just_pressed("interact") or Global.btnGreen) :
 		match area_active[0]:
 			1:
-				get_tree().change_scene("res://Scenes/Levels/Level_01.tscn")
+				if Global.game_progress == 0:
+					get_tree().change_scene("res://Scenes/Levels/Level_01.tscn")
 			2:
-				get_tree().change_scene("res://Scenes/Levels/Level_02.tscn")
+				if Global.game_progress == 1:
+					get_tree().change_scene("res://Scenes/Levels/Level_02.tscn")
 			3:
-				get_tree().change_scene("res://Scenes/Levels/Level_03.tscn")	
+				if Global.game_progress == 2:
+					get_tree().change_scene("res://Scenes/Levels/Level_03.tscn")	
 			4:
-				get_tree().change_scene("res://Scenes/Levels/Level_04.tscn")	
+				if Global.game_progress == 3:
+					get_tree().change_scene("res://Scenes/Levels/Level_04.tscn")	
 			5:
-				get_tree().change_scene("res://Scenes/Levels/Level_05.tscn")	
+				if Global.game_progress == 4:
+					get_tree().change_scene("res://Scenes/Levels/Level_05.tscn")	
 		Global.btnGreen = false
 
 func _on_Level1_body_entered(body):
